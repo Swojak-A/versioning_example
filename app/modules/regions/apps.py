610b9c63bd7adcb286567a43175449d6e992ea13
swojak-a <swojak.a@gmail.com>
2021-01-31 19:30:55 +0100

    feat: add regions module

---
+++ app/modules/regions/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+class RegionsConfig(AppConfig):
+    name = "modules.regions"
+    verbose_name: str = _("Regions Module")
