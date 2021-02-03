97896c59e180592594a5509e06178478a52e8b5b
swojak-a <swojak.a@gmail.com>
2021-02-03 09:03:07 +0100

    feat: add generate-changelog.sh script

---
+++ scripts/generate-changelog.sh
@@ -0,0 +1,4 @@
+auto-changelog --unreleased \
+  --description "Autocrated changelog using generate-changelog script and auto-changelog package." \
+  --tag-pattern '^v[\d]+\.[\d]+\.[\d]' \
+  --starting-commit 8dcd9e
